2f6af9c536dfb32446950313fffc7f99f9fdfee2
Ankit Pati <contact@ankitpati.in>
2018-01-31 12:27:26 +0530

    find smallest integer from input

---
+++ src/smallest.c
@@ -0,0 +1,33 @@
+/* smallest.c */
+/* Date  : 31 January 2018
+ * Author: Ankit Pati
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int min, cur;
+
+    if (scanf(" %d%*c", &cur) == EOF) {
+        fprintf(stderr, "No input given!\n");
+        exit(1);
+    }
+
+    for (min = cur; !feof(stdin) && !ferror(stdin); scanf(" %d%*c", &cur))
+        min = min > cur ? cur : min;
+
+    printf("Minimum = %d\n", min);
+
+    return 0;
+}
+/* end of smallest.c */
+
+/*
+
+43 54 5 -3 54 94
+12 43 2 -9 31 12
+Minimum = -9
+
+*/
