54f11dc40c192cb053b033b86e51c160136e4424
Ankit Pati <contact@ankitpati.in>
2018-01-31 13:31:57 +0530

    vowel or consonant

---
+++ src/vowel.c
@@ -0,0 +1,51 @@
+/* vowel.c */
+/* Date  : 31 January 2018
+ * Author: Ankit Pati
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char c;
+
+    for (;;) {
+        scanf(" %c%*c", &c);
+
+        if (feof(stdin) || ferror(stdin)) break;
+
+        switch (tolower(c)) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+            puts("Vowel");
+            break;
+        default:
+            puts("Consonant");
+            break;
+        }
+
+        putchar('\n');
+    }
+
+    return 0;
+}
+/* end of vowel.c */
+
+/*
+
+q
+Consonant
+
+w
+Consonant
+
+e
+Vowel
+
+a
+Vowel
+
+43
+Consonant
+
+*/
