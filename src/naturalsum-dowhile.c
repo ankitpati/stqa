2843ecf21f4951ae229864c6c55480b488f61cc1
Ankit Pati <contact@ankitpati.in>
2018-02-05 21:07:57 +0530

    sum of first n natural numbers―do…while version

---
+++ src/naturalsum-dowhile.c
@@ -0,0 +1,29 @@
+/* naturalsum-dowhile.c */
+/* Date  : 31 January 2018
+ * Author: Ankit Pati
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int n, sum;
+
+    sum = 0;
+    scanf(" %d%*c", &n);
+
+    do sum += n;
+    while (--n);
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+/* end of naturalsum-dowhile.c */
+
+/* OUTPUT
+
+10
+Sum = 55
+
+*/
