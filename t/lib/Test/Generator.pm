732ff49670ae4917064992efb68d21d48544decd
Ankit Pati <contact@ankitpati.in>
2018-03-25 17:05:08 +0530

    Add Generation of Robustness Tests
    Also add relevant EXPORT_OK entries, and refactor the declaration of
    constants to make it better serve the changing needs of subroutines in
    the module.

--- t/lib/Test/Generator.pm
+++ t/lib/Test/Generator.pm
@@ -7,19 +7,22 @@ use warnings;
 
 use Exporter 'import';
 
-our @EXPORT_OK = qw(boundary_value_tests boundary_value_tests_as_arrayrefs);
-
-use constant {
-    MINIMUM => 0,
-    MINPONE => 1,
-    NOMINAL => 2,
-    MAXMONE => 3,
-    MAXIMUM => 4,
-};
+our @EXPORT_OK = qw(
+    boundary_value_tests boundary_value_tests_as_arrayrefs
+    robustness_tests robustness_tests_as_arrayrefs
+);
 
 sub boundary_value_tests {
     my %bvalues = @_;
     my @tvalues;
+
+    my %c = (
+        minimum => 0,
+        minpone => 1,
+        nominal => 2,
+        maxmone => 3,
+        maximum => 4,
+    );  # constant values for indexing arrays
 
     while (my ($key, $val) = each %bvalues) {
         my ($min, $max) = @$val;
@@ -28,16 +31,18 @@ sub boundary_value_tests {
 
     foreach my $bkey (sort keys %bvalues) {
         # other keys, apart from $bkey, take NOMINAL values
-        my %oth = map { $_ => $bvalues{$_}[NOMINAL] }
+        my %oth = map { $_ => $bvalues{$_}[$c{nominal}] }
                                                 grep !/^$bkey$/, keys %bvalues;
 
         push @tvalues, { $bkey => $_, %oth }
-            foreach @{$bvalues{$bkey}}[MINIMUM, MINPONE, MAXMONE, MAXIMUM];
+            foreach @{$bvalues{$bkey}}[
+                $c{minimum}, $c{minpone}, $c{maxmone}, $c{maximum}
+            ];
                 # exclude NOMINAL value to avoid duplicates
     }
 
     # add NOMINAL values for all variables to make up for the exclusions
-    push @tvalues, { map { $_ => $bvalues{$_}[NOMINAL] } keys %bvalues };
+    push @tvalues, { map { $_ => $bvalues{$_}[$c{nominal}] } keys %bvalues };
 
     return @tvalues;
 }
@@ -46,6 +51,57 @@ sub boundary_value_tests_as_arrayrefs {
     my @tvalues;
 
     foreach my $tval (boundary_value_tests @_) {
+        my @tcase;
+        push @tcase, $tval->{$_} foreach sort keys %$tval;
+        push @tvalues, \@tcase;
+    }
+
+    return @tvalues;
+}
+
+sub robustness_tests {
+    my %bvalues = @_;
+    my @tvalues;
+
+    my %c = (
+        minmone => 0,
+        minimum => 1,
+        minpone => 2,
+        nominal => 3,
+        maxmone => 4,
+        maximum => 5,
+        maxpone => 6,
+    );  # constant values for indexing arrays
+
+    while (my ($key, $val) = each %bvalues) {
+        my ($min, $max) = @$val;
+        $bvalues{$key} = [
+            $min-1, $min, $min+1, int (($min + $max) / 2), $max-1, $max, $max+1
+        ];
+    }
+
+    foreach my $bkey (sort keys %bvalues) {
+        # other keys, apart from $bkey, take NOMINAL values
+        my %oth = map { $_ => $bvalues{$_}[$c{nominal}] }
+                                                grep !/^$bkey$/, keys %bvalues;
+
+        push @tvalues, { $bkey => $_, %oth }
+            foreach @{$bvalues{$bkey}}[
+                $c{minmone}, $c{minimum}, $c{minpone},
+                $c{maxmone}, $c{maximum}, $c{maxpone}
+            ];  # exclude NOMINAL value to avoid duplicates
+    }
+
+    # add NOMINAL values for all variables to make up for the exclusions
+    push @tvalues, { map { $_ => $bvalues{$_}[$c{nominal}] } keys %bvalues };
+
+    return @tvalues;
+}
+
+sub robustness_tests_as_arrayrefs {
+    my @tvalues;
+
+    foreach my $tval (robustness_tests @_) {
         my @tcase;
         push @tcase, $tval->{$_} foreach sort keys %$tval;
         push @tvalues, \@tcase;
